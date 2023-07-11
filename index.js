d040da2da46030900344ba4355542140499d3edb
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-10 18:40:28 -0700

    Set up Node() factory function

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-// Return array of next possible moves given a position on board
-function findNextMoves(x, y) {
-  const array = []
-  for (let xOffset = -2; xOffset <= 2; xOffset++) {
-    if (xOffset === 0) continue
-    let yOffset
-    if (Math.abs(xOffset) === 1) {
-      yOffset = 2
-
-    } else {
-      yOffset = 1
+function Node(x, y) {
+  // Return array of next possible moves given a position on board
+  function findNextMoves(x, y) {
+    const array = []
+    for (let xOffset = -2; xOffset <= 2; xOffset++) {
+      if (xOffset === 0) continue
+      let yOffset
+      if (Math.abs(xOffset) === 1) {
+        yOffset = 2
+      } else {
+        yOffset = 1
+      }
+      array.push([xOffset + x, yOffset + y])
+      array.push([xOffset + x, -yOffset + y])
     }
-    array.push([xOffset + x, yOffset + y])
-    array.push([xOffset + x, -yOffset + y])
+    return array
   }
-  return array
+
+  function getValidMoves(movesArray) {
+    return movesArray.filter(movePosition => {
+      const [x, y] = movePosition
+      return (x >= 0 && y >= 0 && x <= 7 && y <= 7)
+    })
+  }
+
+  function getChildren(x, y) {
+    const possibleMoves = findNextMoves(x, y);
+    const validMoves = getValidMoves(possibleMoves)
+    return validMoves
+  }
+
+  function getPath() {
+
+  }
+
+  return {
+    position: [x, y],
+    pathToCurrent: getPath(),
+    children: getChildren(x, y),
+    findNextMoves: findNextMoves(x, y)
+  }
 }
 
-console.log(findNextMoves(3, 3))
+const node = Node(0, 0)
+console.log(node.children)
+// console.log(findNextMoves(3, 3))
